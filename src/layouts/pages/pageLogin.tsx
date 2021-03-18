8770c010851f7a0244494d2c6ff7849a3fafbe56
30-jubair <jobairt03@gmail.com>
2021-03-18 14:23:55 +0600

    Update pageLogin.tsx

--- src/layouts/pages/pageLogin.tsx
+++ src/layouts/pages/pageLogin.tsx
@@ -9,21 +9,34 @@ export function PageLogin(): JSX.Element {
   const [pass, setPass] = React.useState([]);
 
   return (
-    <div>
-      <div className="form-group">
-        <input type="email" className="form-control" placeholder="Username" />
-      </div>
+    <body className="gray-bg">
+      <div className="middle-box text-center loginscreen animated fadeInDown jumbotron m-5">
+        <div>
+          <div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                placeholder="Email"
+              />
+            </div>
 
-      <div className="form-group">
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Password"
-        />
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+              />
+            </div>
+            <button
+              type="submit"
+              className="btn btn-primary block full-width m-b"
+            >
+              Login
+            </button>
+          </div>
+        </div>
       </div>
-      <button type="submit" className="btn btn-primary block full-width m-b">
-        Login
-      </button>
-    </div>
+    </body>
   );
 }
