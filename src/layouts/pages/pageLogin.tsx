636a203b1af369788528e0bb622c42de92462609
30-jubair <jobairt03@gmail.com>
2021-03-18 13:38:37 +0600

    input field added

--- src/layouts/pages/pageLogin.tsx
+++ src/layouts/pages/pageLogin.tsx
@@ -1,3 +1,29 @@
+import React from "react";
 export function PageLogin(): JSX.Element {
-  return <div>Login Page....</div>;
+  const user = [
+    { email: "abc@gmail.com", password: "12345" },
+    { email: "rafid@gmail.com", password: "67890" },
+  ];
+
+  const [email, setEmail] = React.useState([]);
+  const [pass, setPass] = React.useState([]);
+
+  return (
+    <div>
+      <div className="form-group">
+        <input type="email" className="form-control" placeholder="Username" />
+      </div>
+
+      <div className="form-group">
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Password"
+        />
+      </div>
+      <button type="submit" className="btn btn-primary block full-width m-b">
+        Login
+      </button>
+    </div>
+  );
 }
